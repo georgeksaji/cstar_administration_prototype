9d03603282187f098515bbda343596d0fda57edf
georgeksaji <georgeksaji14@gmail.com>
2025-07-23 00:03:43 +0530

    Removed git ignores

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+<<<<<<< HEAD
+  // base: "/cstar_administration_prototype/",
+});
+=======
+   base: "/cstar_administration_prototype/", 
+})
+>>>>>>> 93f99f49297cb2b5f4eed5b8a46a759201aa55a8
